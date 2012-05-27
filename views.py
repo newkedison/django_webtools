402943b18010240a77359aff06e183600e751d2f
newk <newkedison@gmail.com>
2012-05-27 19:30:00 +0800

    make the robots.txt seem more like a real file(OK, it exactly is a real file)

--- views.py
+++ views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse
 
 def index(request):
   return render_to_response('index.html')
 
 def robots(request):
-  return render_to_response('robots.txt')
+  import os
+  s = open(os.path.join(os.path.dirname(__file__), "templates/robots.txt"), "rb").read()
+  response = HttpResponse(s, mimetype='text/plain')
+#if add the below line, then the browser will open a download window instead of showing the content of file
+#  response['Content-Disposition'] = 'attachment; filename=robots.txt'
+  return response
