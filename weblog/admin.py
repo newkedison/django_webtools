f05fbb4cefa7749ec151642a201c7f98749b3bc6
newk <newkedison@gmail.com>
2012-06-03 09:16:50 +0800

    make the admin.py for weblog app simply

--- weblog/admin.py
+++ weblog/admin.py
@@ -1,15 +1,6 @@
 from weblog.models import Person, Action, Log
 from django.contrib import admin
 
-class PersonAdmin(admin.ModelAdmin):
-  pass
-
-class ActionAdmin(admin.ModelAdmin):
-  pass
-
-class LogAdmin(admin.ModelAdmin):
-  pass
-
-admin.site.register(Person, PersonAdmin)
-admin.site.register(Action, ActionAdmin)
-admin.site.register(Log, LogAdmin)
+admin.site.register(Person)
+admin.site.register(Action)
+admin.site.register(Log)
